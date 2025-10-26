be80f4f15dbfd47312a15f881deceab43d08823e
Jagdish Patil <patil.jagdish222@gmail.com>
2025-10-26 10:22:33 +0530

    new interview ex added

---
+++ Javascript/import-export/index.js
@@ -0,0 +1,6 @@
+import { add, subtract } from "./math.js";
+
+const addRes = add(2, 30);
+const subtractRes = subtract(2, 30);
+console.log("addition result", addRes);
+console.log("sub result", subtractRes);
